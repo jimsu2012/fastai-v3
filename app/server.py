72c71e02a46bd4164e853ce53aea49d6836621e7
jimsu2012 <46636043+jimsu2012@users.noreply.github.com>
2019-05-17 22:40:16 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
 export_file_url = 'https://drive.google.com/uc?export=download&id=1QJ6UnRmTTph7tUvoS2jllKC3cyOQf3hk'
-export_file_name = 'er-4.pth'
+export_file_name = 'er-4'
 
 classes = ['man', 'woman']
 path = Path(__file__).parent
