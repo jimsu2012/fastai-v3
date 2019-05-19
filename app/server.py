ae6d8e19171baa8a6e4eb462db3dd3ce6b4bd6a4
jimsu2012 <46636043+jimsu2012@users.noreply.github.com>
2019-05-19 14:42:11 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=1QJ6UnRmTTph7tUvoS2jllKC3cyOQf3hk'
-export_file_name = 'er-4'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-3CDYWmYbF2W4xj-b1IicMwg1YhP5IPz'
+export_file_name = 'er-3.pkl'
 
 classes = ['man', 'woman']
 path = Path(__file__).parent
