04959b68135a4a08e7d24b97be9f4a928c51fb7a
jimsu2012 <46636043+jimsu2012@users.noreply.github.com>
2019-05-19 18:51:35 -0500

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -24,7 +24,7 @@ function analyze() {
     xhr.onload = function(e) {
         if (this.readyState === 4) {
             var response = JSON.parse(e.target.responseText);
-            el('result-label').innerHTML = `Result = ${response['result']}`;
+            el('result-text').innerHTML = `You are a ${response['result']}!`;
         }
         el('analyze-button').innerHTML = 'Analyze';
     }
